d5ca2a3cab2366a2d46901596782d6aa9ed335aa
pattlebass <49322676+pattlebass@users.noreply.github.com>
2021-10-29 00:23:42 +0300

    Better FPS controller
    * Static typing
    * Ignore y camera rotation in movement

--- Tools/FPS controller/player.gd
+++ Tools/FPS controller/player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody
 
 onready var camera = $Pivot/Camera
+onready var interact_ray = $Pivot/Camera/RayCast
+onready var inventory = $Inventory
 
 const GRAVITY = -30
 const MAX_WALK_SPEED = 8
@@ -11,68 +13,63 @@ const JUMP_SPEED = 12
 var mouse_sensitivity = 0.002  # radians/pixel
 var first_person = true
 
-var velocity:Vector3 = Vector3()
-var jump = false
-var sprinting = false
+var velocity := Vector3()
+var jump := false
+var sprinting := false
 
+var hover_object
 
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-func get_input_direction():
-	jump = false
-	if Input.is_action_just_pressed("Jump"):
-		jump = true
-	if Input.is_action_pressed("Sprint"):
-		sprinting = true
-	else:
-		sprinting = false
-	
-	
-	var input_direction = Vector3()
-	
-	if Input.is_action_pressed("Forward"):
-		input_direction += -camera.global_transform.basis.z
-	if Input.is_action_pressed("Backward"):
-		input_direction += camera.global_transform.basis.z
-	if Input.is_action_pressed("Left"):
-		input_direction += -camera.global_transform.basis.x
-	if Input.is_action_pressed("Right"):
-		input_direction += camera.global_transform.basis.x
-	
-	input_direction = input_direction.normalized()
-	
-	return input_direction
-
+func _physics_process(delta) -> void:
+	movement(delta)
 
 func _unhandled_input(event):
 	# Camera rotation
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
-		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
+		$Pivot.rotation_degrees.x = clamp(
+			$Pivot.rotation_degrees.x,
+			-85,
+			85
+		)
 	# Camera positions
-	if event.is_action_pressed("Change_camera"):
+	if event.is_action_pressed("change_camera"):
 		first_person = !first_person
-		
 		if first_person:
 			camera.translation = Vector3.ZERO
 		else:
 			camera.translation = Vector3(0.75, 0, 2.25)
 	
 	# Mouse capturing
-	if event.is_action_pressed("Pause"):
+	if event.is_action_pressed("pause"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	elif event.is_action_pressed("Left_click"):
+	elif event.is_action_pressed("left_click"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+	var hover_object = interact_ray.get_collider()
+	if hover_object:
+		var interaction_text = ("[%s] " + hover_object.get_interaction_text()) \
+			% InputMap.get_action_list("interact")[0].as_text()
+		Variables.interact_label.text = interaction_text
+		
+		if event.is_action_pressed("interact"):
+			hover_object.on_interact()
+	else:
+		Variables.interact_label.text = ""
+
+
+func movement(delta) -> void:
+	jump = false
+	if Input.is_action_just_pressed("jump"):
+		jump = true
+	if Input.is_action_pressed("sprint"):
+		sprinting = true
+	else:
+		sprinting = false
 	
-func _physics_process(delta):
-	movement(delta)
-
-
-func movement(delta):
 	velocity.y += GRAVITY * delta
 	var desired_velocity = get_input_direction()
 	
@@ -87,5 +84,21 @@ func movement(delta):
 	velocity.z = lerp(velocity.z, desired_velocity.z, ACCELERATION)
 	
 	velocity = move_and_slide(velocity, Vector3.UP, true)
+
+
+func get_input_direction() -> Vector3:
+	var input_direction := Vector3()
 	
+	if Input.is_action_pressed("forward"):
+		input_direction += -camera.global_transform.basis.z
+	if Input.is_action_pressed("backward"):
+		input_direction += camera.global_transform.basis.z
+	if Input.is_action_pressed("left"):
+		input_direction += -camera.global_transform.basis.x
+	if Input.is_action_pressed("right"):
+		input_direction += camera.global_transform.basis.x
 	
+	input_direction.y = 0 # Else the player controls like a spectator
+	input_direction = input_direction.normalized()
+
+	return input_direction
